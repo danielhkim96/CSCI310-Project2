1cbcdade8f615dcc81ef4e9274770cb4a31e0153
samuel-he <hes@usc.edu>
2019-04-12 20:37:19 -0800

    fixed database

--- database.sql
+++ database.sql
@@ -8,36 +8,29 @@ CREATE TABLE User (
 	password VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE Restaurant (
-	restaurantName		VARCHAR(100)	PRIMARY KEY,
+
+CREATE TABLE ListRestaurants (
+	listName		VARCHAR(100) 	PRIMARY KEY
+	username		VARCHAR(100)	NOT NULL,
+	listIndex		VARCHAR(100)	NOT NULL,
+	restaurantName		VARCHAR(100)	NOT NULL,
 	restaurantAddress	VARCHAR(100)	NOT NULL,
 	restaurantPhone		VARCHAR(100)	NOT NULL,
 	restaurantURL		VARCHAR(100)	NOT NULL
 	restaurantRating 	VARCHAR(100)	NOT NULL
 	restaurantDriveTime	VARCHAR(100)	NOT NULL
-);
-
-CREATE TABLE Recipe (
-	recipeName			VARCHAR(100)	PRIMARY KEY,
-	recipeImageURL		VARCHAR(100) 	NOT NULL,
-	recipeCookTime		VARCHAR(50) 	NOT NULL,
-	recipePrepTime		VARCHAR(50)		NOT NULL,
-	recipeInstructions	VARCHAR(1000)	NOT NULL
-	recipeIngredients	VARCHAR(1000)	NOT NULL
-);
-
-CREATE TABLE ListRestaurants (
-	listName	VARCHAR(100) 	PRIMARY KEY
-	username		VARCHAR(100)	NOT NULL,
-	restaurantID	INT(11)			NOT NULL,
 	FOREIGN KEY (username) REFERENCES User(username),
-	FOREIGN KEY (restaurantName) REFERENCES Restaurant(restaurantName)
 );
 
 CREATE TABLE ListRecipes (
 	listName	VARCHAR(100) 	PRIMARY KEY
 	username	VARCHAR(100)	NOT NULL,
-	recipeID	INT(11)			NOT NULL,
+	listIndex		VARCHAR(100)	NOT NULL,
+	recipeName			VARCHAR(100) 	NOT NULL,
+	recipeImageURL		VARCHAR(100) 	NOT NULL,
+	recipeCookTime		VARCHAR(50) 	NOT NULL,
+	recipePrepTime		VARCHAR(50)		NOT NULL,
+	recipeInstructions	VARCHAR(1000)	NOT NULL
+	recipeIngredients	VARCHAR(1000)	NOT NULL
 	FOREIGN KEY (username) REFERENCES User(username),
-	FOREIGN KEY (recipeName) REFERENCES Recipe(recipeName)
 );
