7658bdd075d2eaaeadacbdadd3ada33973860abe
Yvette <yvettelo@usc.edu>
2019-04-12 20:04:17 -0800

    Committing on Daniel's behalf: Added login functionality

---
+++ src/main/java/edu/usc/cs/group8/ImHungry/LoginServlet.java
@@ -0,0 +1,84 @@
+package edu.usc.cs.group8.ImHungry;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+    
+    @Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	Connection conn = null;
+		ResultSet rs = null;
+		PreparedStatement ps = null;
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/project2?user=root&password=12345&userSSL=false");
+			
+			String username = request.getParameter("email");
+			String password = request.getParameter("password");
+			String action = request.getParameter("action");
+			
+			PrintWriter out = response.getWriter();
+			
+			if (action.equals("register")) {
+				ps = conn.prepareStatement("INSERT INTO User (username, password) VALUES (?,?)");
+				ps.setString(1, username);
+				ps.setString(2, password);
+				int update = ps.executeUpdate();
+				if (update == 1) {
+					out.println("Registration successful");
+					request.getSession().setAttribute("username", username);
+					response.sendRedirect(request.getContextPath() + "/search_page.jsp");
+				} else {
+					out.println("Registration failed");
+				}
+			} else {
+				ps = conn.prepareStatement("SELECT * FROM User WHERE username = ?");
+				ps.setString(1, username);
+				
+				rs = ps.executeQuery();
+				if (rs.next()) {
+					if (password.equals(rs.getString("password"))) {
+						out.println("Login successful");
+						request.getSession().setAttribute("username", username);
+						response.sendRedirect(request.getContextPath() + "/search_page.jsp");
+					} else {
+						out.println("Incorrect password");
+					}
+				}
+			}
+			
+//			ps = conn.prepareStatement("SELECT * FROM User");
+//			rs = ps.executeQuery();
+//			while (rs.next()) {
+//				String un = rs.getString("username");
+//				String pw = rs.getString("password");
+//				System.out.println("Username: " + un);
+//				System.out.println("Password: " + pw + "\n");
+//			}
+		} catch (SQLException sqle) {
+			sqle.printStackTrace();
+		} catch (ClassNotFoundException cnfe) {
+			cnfe.printStackTrace();
+		}
+    }
+
+}
