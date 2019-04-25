99c12ca3da8e30c14c6db2d9a96d4d4f89c52dc3
samuel-he <hes@usc.edu>
2019-04-24 22:01:15 -0700

    Sam and Hiro Pair Programming 4/24 : changed loginServlet to aid in data persistence, modifed database schema and IHManageList accordingly

--- database.sql
+++ database.sql
@@ -32,13 +32,30 @@ CREATE TABLE ListRecipes (
 	recipeImageURL		VARCHAR(300) 	NOT NULL,
 	recipeCookTime		VARCHAR(50) 	NOT NULL,
 	recipePrepTime		VARCHAR(50)		NOT NULL,
-	recipeInstructions	VARCHAR(5000)	NOT NULL,
-	recipeIngredients	VARCHAR(5000)	NOT NULL,
 	FOREIGN KEY (username) REFERENCES User(username)
 );
 
 CREATE TABLE Grocery (
 	ingredientName	VARCHAR(100) 	PRIMARY KEY,
 	username	VARCHAR(100)	NOT NULL,
+	listIndex	VARCHAR(100)	NOT NULL,
 	FOREIGN KEY (username) REFERENCES User(username)
+);
+
+CREATE TABLE Instructions (
+	instruction	VARCHAR(1000) 	PRIMARY KEY,
+	username	VARCHAR(100)	NOT NULL,
+	recipeName		VARCHAR(100)	NOT NULL,
+	listIndex	VARCHAR(100)	NOT NULL,
+	FOREIGN KEY (username) REFERENCES User(username),
+	FOREIGN KEY (recipeName) REFERENCES User(recipeName)
+);
+
+CREATE TABLE Ingredients (
+	ingredient	VARCHAR(1000) 	PRIMARY KEY,
+	username	VARCHAR(100)	NOT NULL,
+	recipeName		VARCHAR(100)	NOT NULL,
+	listIndex	VARCHAR(100)	NOT NULL,
+	FOREIGN KEY (username) REFERENCES User(username),
+	FOREIGN KEY (recipeName) REFERENCES User(recipeName)
 );
